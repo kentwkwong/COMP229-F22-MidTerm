de23787037f29e6e1330812c870846e4937cc0d1
Kent Wong <kentwkwong@gmail.com>
2022-11-01 21:08:13 -0400

    COMP229 Fall22 Midterm test

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://dbconnectuser:eyuyLGvI97LihnQi@mongodbserver.uq7npjq.mongodb.net/F22MidTerm?retryWrites=true&w=majority"
+};
+
+
+/*
+{
+  "Title": "",
+  "Description": "",
+  "Price": 0,
+  "Author": "",
+  "Genre": ""
+}
+*/
